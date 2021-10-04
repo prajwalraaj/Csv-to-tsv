99fde9d83cff5a2237fab624939a4706b966ceb0
prajwalraaj <77952023+prajwalraaj@users.noreply.github.com>
2021-10-04 10:09:44 +0530

    Create csvtotsv.py

---
+++ csvtotsv.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('Data.csv','r') as csvin, open('Data.tsv', 'w') as tsvout:
+    csvin = csv.reader(csvin)
+    tsvout = csv.writer(tsvout, delimiter='\t')
+
+    for row in csvin:
+        tsvout.writerow(row)
